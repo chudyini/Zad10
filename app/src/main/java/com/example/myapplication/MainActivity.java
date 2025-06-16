1c925d2ca68352ff3078540a46458089c00d9f51
9601941u <majewskikuba13@gmail.com>
2025-06-16 13:28:27 +0200

    dfgg

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,60 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    EditText daneWlasciciela;
+    TextView gatuneczek;
+    Spinner mySimpleSpinner;
+    TextView ileLat;
+    TextView aktualnyWiek;
+    SeekBar wiekSeekBar;
+    EditText celWizyty;
+    TimePicker timePickerr;
+    Button przycisk;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        daneWlasciciela =findViewById(R.id.daneWlasciciela);
+        gatuneczek =findViewById(R.id.gatuneczek);
+        mySimpleSpinner =findViewById(R.id.mySimpleSpinner);
+        ileLat =findViewById(R.id.ileLat);
+        aktualnyWiek =findViewById(R.id.ileLat);
+        wiekSeekBar =findViewById(R.id.wiekSeekBar);
+        celWizyty =findViewById(R.id.celWizyty);
+        timePickerr = findViewById(R.id.timePickerr);
+        przycisk = findViewById(R.id.przycisk);
+        timePickerr.setHour(16);
+        timePickerr.setMinute(0);
+
+        wiekSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    ileLat.setText("" + progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
         });
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_options_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySimpleSpinner.setAdapter(adapter);
+
+
+
+
+
     }
 }
